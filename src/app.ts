ebf53eb540c95a5c653af980ae5f4ec901c2bb7c
Chandanbir <chandanbir.singh@marblex.ai>
2025-08-21 15:14:53 +0530

    feat: add CORS support and update dependencies

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import cors from 'cors';
 
 import formRouter from './routes/form.routes';
 import questionRouter from './routes/question.routes';
@@ -7,6 +8,8 @@ import responseRouter from './routes/response.routes';
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
